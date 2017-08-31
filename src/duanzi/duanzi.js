ecc1b1f059534de466d8bd95ed1b3be69d85482d
wk633 <wk633@outlook.com>
2017-08-31 13:45:11 -0700

    fix infinity loading bug and adjust to real api

--- src/duanzi/duanzi.js
+++ src/duanzi/duanzi.js
@@ -7,6 +7,8 @@ import fakeData from './fakeData';
 import DuanziItem from './duanziItem';
 
 const data = fakeData;
+const DUANZI_PER_PAGE = 20;
+const API_BASE = "http://localhost:4000/api/v1/duanzi/"
 
 class Duanzi extends Component {
     constructor() {
@@ -15,31 +17,55 @@ class Duanzi extends Component {
             duanzi: [],
             offSet: 0,
             loadedAll: false,
-            loading: false
+            loading: true
         }
         this.handleScroll = this.handleScroll.bind(this);
     }
     componentDidMount() {
         console.log("component did mount");
         this.loadData();
-        this.loadData = _.debounce(this.loadData, 500);
+        this.handleScroll = _.debounce(this.handleScroll, 500);
         window.addEventListener('scroll', this.handleScroll);
         console.log(this.state);
     }
     handleScroll() {
         let scrollY = window.scrollY || window.pageYOffset || document.documentElement.scrollTop;
-        if (window.innerHeight + scrollY >= (document.body.offsetHeight - 50)) {
+        if (window.innerHeight + scrollY >= (document.documentElement.scrollHeight - 50)) {
+            console.log(window.innerHeight, scrollY, document.documentElement.scrollHeight);
             console.log("loading more duanzi");
-            this.setState({
-                loading: true
-            });
+
             this.loadData();
         }
     }
     loadData() {
-        this.setState({
-            duanzi: data
-        })
+        // this.state.offSet then update this.state.offSet
+        if (this.state.loadedAll == true) {
+            return;
+        }
+
+        let url = API_BASE + this.state.offSet;
+        let request = new Request(
+            encodeURI(url),
+            {
+                method: 'GET',
+                cache: false
+            }
+        );
+        fetch(request)
+            .then(res => res.json())
+            .then(response => {
+                console.log(response);
+                if (!response || response.length == 0) {
+                    this.setState({
+                        loadedAll: true
+                    });
+                }else {
+                    this.setState({
+                        duanzi: this.state.duanzi.concat(response),
+                        offSet: this.state.offSet + DUANZI_PER_PAGE
+                    });
+                }
+            })
     }
 
     loading() {
